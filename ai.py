78072c2263571dd52418ba16f133ff991be27076
tdolega <tymoteuszdolega@gmail.com>
2022-05-31 20:00:31 +0200

    improve code readability

--- ai.py
+++ ai.py
@@ -1,5 +1,6 @@
 from globalSetting import *
 from analyze import N_INPUT_SIZE
+import math
 
 class Network(torch.nn.Module):
     def __init__(self, output_w=None):
@@ -25,6 +26,7 @@ class Network(torch.nn.Module):
 class Population:
     def __init__(self, size, lastPopulation=None):
         self.size = size
+        self.fitnesses = np.zeros(self.size)
         if lastPopulation is None:
             self.models = [Network() for _ in range(size)]
         else:
@@ -33,50 +35,43 @@ class Population:
             self.models = []
             self.crossover()
             self.mutate()
-        self.fitnesses = np.zeros(self.size)
 
     def crossover(self):
         logging.info('Crossover')
-        sum_fitnesses = np.sum(self.lastFitnesses)
-        probs = [self.lastFitnesses[i] / sum_fitnesses for i in range(self.size)]
+        fitnessSum = np.sum(self.lastFitnesses)
+        probabilities = [self.lastFitnesses[i] / fitnessSum for i in range(self.size)]
 
-        # Sorting descending NNs according to their fitnesses
-        sort_indices = np.argsort(probs)[::-1]
-        for i in range(self.size):
-            if i < self.size * P_ELITISM:
-                # Add the top performing childs
-                model_c = self.lastModels[sort_indices[i]]
-            else:
-                a, b = np.random.choice(self.size, size=2, p=probs, replace=False)
+        sortedProbabilities = np.argsort(probabilities)[::-1]
+        boundary = math.floor(self.size * P_ELITISM)
+        for i in range(boundary):
+            modelNext = self.lastModels[sortedProbabilities[i]]
+            self.models.append(modelNext)
+        for i in range(boundary, self.size):
+            randA, randB = np.random.choice(self.size, size=2, p=probabilities, replace=False)
+            modelA, modelB = self.lastModels[randA], self.lastModels[randB]
+            modelNext = Network()
 
-                model_a, model_b = self.lastModels[a], self.lastModels[b]
-                model_c = Network()
+            for inputIdx in range(N_INPUT_SIZE):
+                randomModel = np.random.choice([modelA, modelB])
+                modelNext.middle.weight.data[0][inputIdx] = randomModel.middle.weight.data[0][inputIdx]
+            for inputIdx in range(N_MIDDLE_SIZE):
+                randomModel = np.random.choice([modelA, modelB])
+                modelNext.output.weight.data[0][inputIdx] = randomModel.output.weight.data[0][inputIdx]
 
-                for inputIdx in range(N_INPUT_SIZE):
-                    randomModel = np.random.choice([model_a, model_b])
-                    model_c.middle.weight.data[0][inputIdx] = randomModel.middle.weight.data[0][inputIdx]
-                for inputIdx in range(N_MIDDLE_SIZE):
-                    randomModel = np.random.choice([model_a, model_b])
-                    model_c.output.weight.data[0][inputIdx] = randomModel.output.weight.data[0][inputIdx]
+            self.models.append(modelNext)
 
-            self.models.append(model_c)
+    def _mutateSingle(self, weight):
+        if np.random.random() > P_MUTATION:
+            return
+
+        with torch.no_grad():
+            noise = torch.randn(1).mul_(P_WEIGHTS_MUTATE_POWER).to(N_DEVICE)
+            weight.add_(noise[0])
 
     def mutate(self):
         logging.info('Mutating')
         for model in self.models:
-            # Mutating weights by adding Gaussian noises
             for i in range(N_INPUT_SIZE):
-                if np.random.random() > P_MUTATION:
-                    continue
-
-                with torch.no_grad():
-                    noise = torch.randn(1).mul_(P_WEIGHTS_MUTATE_POWER).to(N_DEVICE)
-                    model.middle.weight.data[0][i].add_(noise[0])
-
+                self._mutateSingle(model.middle.weight.data[0][i])
             for i in range(N_MIDDLE_SIZE):
-                if np.random.random() > P_MUTATION:
-                    continue
-
-                with torch.no_grad():
-                    noise = torch.randn(1).mul_(P_WEIGHTS_MUTATE_POWER).to(N_DEVICE)
-                    model.output.weight.data[0][i].add_(noise[0])
+                self._mutateSingle(model.output.weight.data[0][i])
